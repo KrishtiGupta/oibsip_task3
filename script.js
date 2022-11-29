2959078d46c09761ba513778dc69451901568b96
KrishtiGupta <krishtigupta6392@gmail.com>
2022-11-29 19:57:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('convBtn').onclick = Convert;
+document.getElementById('clearBtn').onclick = clear;
+
+
+function Convert() {
+    var fahrenheit = document.getElementById("fah").value;
+    var celsius = document.getElementById("cel").value;
+  if (fahrenheit != '') {
+        celsius = (parseFloat(fahrenheit) - 32) / 1.8;
+    } else {
+        fahrenheit = (parseFloat(celsius) * 1.8) + 32;
+    }
+    document.getElementById('fah').value = parseFloat(fahrenheit).toFixed(1);
+    document.getElementById('cel').value = parseFloat(celsius).toFixed(1);
+}
+
+
+function clear() {
+    document.getElementById('fah').value = '';
+    document.getElementById('cel').value = '';
+}
